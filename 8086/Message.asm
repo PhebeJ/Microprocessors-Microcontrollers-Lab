926c94354a2c97160fcc41b65736ff16684b383d
Phebe Joshua <feba.j7@gmail.com>
2021-07-20 11:41:30 +0530

    Create Message.asm

---
+++ 8086/Message.asm
@@ -0,0 +1,14 @@
+ASSUME CS:CODE,DS:DATA
+DATA SEGMENT
+    msg db "Hello World$"
+DATA ENDS
+CODE SEGMENT
+START: MOV AX,DATA
+       MOV DS,AX
+       LEA DX,msg
+       MOV AH,09H
+       INT 21H
+       MOV AH,4CH
+       INT 21H
+CODE ENDS
+END START
